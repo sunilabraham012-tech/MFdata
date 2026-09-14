6bb0f894e59428b0f2ac265e763c55da2b33984b
Abe <sunilabraham012@gmail.com>
2026-09-14 10:31:11 +0530

    Before overall Exceptions as e:

--- src/extractor/client.py
+++ src/extractor/client.py
@@ -6,9 +6,22 @@ from config import API_BASE_URL, API_KEY
 def search_funds(fund):
     url = f"{API_BASE_URL}/api/search"
     headers = {"x-api-key":API_KEY}
-    api_data = requests.get(url,headers=headers, timeout=10, params={"q":fund})
-    return api_data.json()
+    try:
+        api_data = requests.get(url,headers=headers, timeout=10, params={"q":fund})
+        api_data.raise_for_status()
+    except requests.exceptions.Timeout:
+        print("Request timed out after 10 seconds.")
+        return None
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        print("HTTP Error!!")
+        return None
+        
+    api_result = api_data.json()
 
-a = search_funds('hdfc')
-# print(a)
-print(a)
+    if api_result['error'] is None:
+        return api_result['data']
+    else:
+        print(api_result['error'])
+        return None 
+    
---
+++ src/extractor/main.py
@@ -0,0 +1,10 @@
+from client import search_funds
+
+api_data = search_funds('parag')
+print(api_data)
+# print(len(api_data))
+
+
+
+
+
