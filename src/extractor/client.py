ac341b09958a03c6dc81d9d8bac2d01abcd2bc29
Abe <sunilabraham012@gmail.com>
2026-09-14 13:49:01 +0530

    feat: error handling implemented to src code

--- src/extractor/client.py
+++ src/extractor/client.py
@@ -1,4 +1,5 @@
 import requests
+import traceback
 
 from config import API_BASE_URL, API_KEY
 
@@ -9,19 +10,20 @@ def search_funds(fund):
     try:
         api_data = requests.get(url,headers=headers, timeout=10, params={"q":fund})
         api_data.raise_for_status()
+        api_result = api_data.json()
+
+        if api_result['error'] is None:
+            return api_result['data']
+        else:
+            print(api_result['error'])
+            return None 
+
     except requests.exceptions.Timeout:
         print("Request timed out after 10 seconds.")
         return None
     except requests.exceptions.HTTPError as e:
-        print(e)
-        print("HTTP Error!!")
+        traceback.print_exc()
+        return None  
+    except requests.exceptions.RequestException as e:
+        traceback.print_exc()
         return None
-        
-    api_result = api_data.json()
-
-    if api_result['error'] is None:
-        return api_result['data']
-    else:
-        print(api_result['error'])
-        return None 
-    
--- src/extractor/main.py
+++ src/extractor/main.py
@@ -1,8 +1,7 @@
 from client import search_funds
 
 api_data = search_funds('parag')
-print(api_data)
-# print(len(api_data))
+print(api_data) 
 
 
 
