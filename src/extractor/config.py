27e1cfbe5cba69f60b154835a25c125622238576
Abe <sunilabraham012@gmail.com>
2026-09-24 23:46:13 +0530

    feat: API results are written into Snowflake DB

--- src/extractor/config.py
+++ src/extractor/config.py
@@ -1,8 +1,17 @@
 from dotenv import load_dotenv
 import os
 
+# Step 1: Load the values from .env into the environment
 load_dotenv()
 
+#API Credentials
 API_BASE_URL = os.getenv("API_BASE_URL")
-
 API_KEY = os.getenv("API_KEY")
+
+#Snowflake Credentials
+user = os.getenv("SNOWFLAKE_USER")
+password = os.getenv("SNOWFLAKE_PASSWORD")
+account = os.getenv("SNOWFLAKE_ACCOUNT")
+warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
+database = os.getenv("SNOWFLAKE_DATABASE")
+schema = os.getenv("SNOWFLAKE_SCHEMA")
